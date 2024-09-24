70536086e88074b5c3d621169e8d8bd8b4e04cf7
Shalahur <srpolash1991@gmail.com>
2024-09-24 19:59:54 +0600

    Create database and table using migration and EF core

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data;
 
@@ -8,5 +9,7 @@ public class ApplicationDbContext : DbContext
     {
         
     }
+    
+    public DbSet<Category> Categories { get; set; }
 
 }
