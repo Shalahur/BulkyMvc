b5493376f9e0babee4985cb9f2f73cb6e502ed66
Shalahur <srpolash1991@gmail.com>
2024-10-06 23:39:29 +0600

    Order tables

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -15,6 +15,8 @@ public class ApplicationDbContext : IdentityDbContext
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     public DbSet<Company> Companies { get; set; }
     public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -118,11 +120,15 @@ public class ApplicationDbContext : IdentityDbContext
                 ImageUrl = ""
             }
         );
-        
+
         modelBuilder.Entity<Company>().HasData(
-            new Company { Id = 1, Name = "Tech Solution", StreetAddress="123 Tech St", City="Tech City",
-                PostalCode="12121", State="IL", PhoneNumber="6669990000"},
-            new Company {
+            new Company
+            {
+                Id = 1, Name = "Tech Solution", StreetAddress = "123 Tech St", City = "Tech City",
+                PostalCode = "12121", State = "IL", PhoneNumber = "6669990000"
+            },
+            new Company
+            {
                 Id = 2,
                 Name = "Vivid Books",
                 StreetAddress = "999 Vid St",
@@ -131,7 +137,8 @@ public class ApplicationDbContext : IdentityDbContext
                 State = "IL",
                 PhoneNumber = "7779990000"
             },
-            new Company {
+            new Company
+            {
                 Id = 3,
                 Name = "Readers Club",
                 StreetAddress = "999 Main St",
