9a9b24ef33dad011fa7779d8565ffa92fdbcd05c
Shalahur <srpolash1991@gmail.com>
2024-10-06 22:35:02 +0600

    shopping cart

--- Bulky.DataAccess/Repository/Repository.cs
+++ Bulky.DataAccess/Repository/Repository.cs
@@ -17,9 +17,15 @@ public class Repository<T> : IRepository<T> where T : class
         _db.Products.Include(p => p.Category).Include(p => p.CategoryId);
     }
 
-    public IEnumerable<T> GetAll(string? includeProperties = null)
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
     {
         IQueryable<T> query = dbSet;
+        
+        if (filter is not null)
+        {
+            query = query.Where(filter);
+        }
+
         if (!string.IsNullOrEmpty(includeProperties))
         {
             foreach (var includeProperty in includeProperties.Split(new char[] { ',' },
